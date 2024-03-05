80a525ef78d01335931c743dac00e8ad48704bed
Alan Kleber <alan.kleber@scalefast.com>
2024-03-05 21:21:17 +0100

    model

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -13,6 +13,16 @@ type Snippet struct {
 	Expires time.Time
 }
 
-type SnippetMOdel struct {
+type SnippetModel struct {
 	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
 }
